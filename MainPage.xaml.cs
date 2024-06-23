8946fe1d6206ffa865f9addb814d9b3fd7824455
JoseCuadrosAlvarez <149647458+JoseCuadrosAlvarez@users.noreply.github.com>
2024-06-23 18:06:24 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XAMARIN_GENERATE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MostrarCV();
+        }
+
+        private void MostrarCV()
+        {
+            string nombre = "José";
+            string apellido = "Cuadros";
+            string email = "josefrances06@gmail.com";
+            string universidad = "San Pablo";
+            string carrera = "Ciencia de la Computación";
+            string telefono = "934887689";
+
+            CVStackLayout.Children.Clear();
+
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = "Curriculum Vitae",
+                Style = (Style)Resources["TitleLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Nombre: {nombre}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Apellido: {apellido}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Email: {email}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Universidad: {universidad}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Carrera: {carrera}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+            CVStackLayout.Children.Add(new Label
+            {
+                Text = $"Teléfono: {telefono}",
+                Style = (Style)Resources["CVLabelStyle"]
+            });
+        }
+    }
+}
---
+++ Pag1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XAMARIN_GENERATE
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
